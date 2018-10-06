dca8aa4edcec81b356bd8670befefd57888b5be1
Sattaya Singkul <59070173@it.kmitl.ac.th>
2018-10-06 14:56:41 +0700

    add main vue structure (Pet)

---
+++ vue_pet/src/components/EditProfile.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="new-profile">
+    <h3>Edit Profile</h3>
+    <div class="row">
+    <form @submit.prevent="updateProfile" class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <input type="text" v-model="profile_id" required>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input type="text" v-model="profile_name" required>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input type="text" v-model="dept" required>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input type="text" v-model="position" required>
+        </div>
+      </div>
+      <button type="submit" class="btn">Submit</button>
+      <router-link to="/" class="btn grey">Cancel</router-link>
+    </form>
+  </div>
+  </div>
+</template>
+
+<script>
+  import db from './firebaseInit'
+  export default {
+    name: 'edit-profile',
+    data () {
+      return {
+        profile_id: null,
+        profile_name: null,
+        dept: null,
+        position: null
+      }
+    },
+    beforeRouteEnter (to, from, next) {
+      db.collection('profiles').where('profile_id', '==', to.params.profile_id).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          next(vm => {
+            vm.profile_id = doc.data().profile_id
+            vm.profile_name = doc.data().profile_name
+            vm.dept = doc.data().dept
+            vm.position = doc.data().position
+          })
+        })
+      })
+    },
+    watch: {
+      '$route': 'fetchData'
+    },
+    methods: {
+      fetchData () {
+        db.collection('profiles').where('profile_id', '==', this.$route.params.profile_id).get().then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            this.profile_id = doc.data().profile_id
+            this.profile_name = doc.data().profile_name
+            this.dept = doc.data().dept
+            this.position = doc.data().position
+          })
+        })
+      },
+      updateProfile () {
+        db.collection('profiles').where('profile_id', '==', this.$route.params.profile_id).get().then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            doc.ref.update({
+              profile_id: this.profile_id,
+              profile_name: this.profile_name,
+              dept: this.dept,
+              position: this.position
+            })
+            .then(() => {
+              this.$router.push({ profile_name: 'view-profile', params: { profile_id: this.profile_id }})
+            });
+          })
+        })
+      }
+    }
+  }
+</script>
---
+++ vue_pet/src/components/ViewPet.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="view-pet">
+    <ul class="collection with-header">
+      <li class="collection-header"><h4>{{pet_name}}</h4></li>
+      <li class="collection-item">Pet ID#: {{pet_id}}</li>
+      <li class="collection-item">Department: {{dept}}</li>
+      <li class="collection-item">Position: {{position}}</li>
+    </ul>
+    <router-link to="/" class="btn grey">Back</router-link>
+    <button @click="deletePet" class="btn red">Delete</button>
+
+    <div class="fixed-action-btn">
+      <router-link v-bind:to="{ name: 'edit-pet', params: { pet_id: pet_id }}" class="btn-floating btn-large red">
+        <i class="fa fa-pencil"></i>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import db from './firebaseInit';
+export default {
+  name: 'view-pet',
+  data() {
+    return {
+      pet_id: null,
+      pet_name: null,
+      dept: null,
+      position: null
+    };
+  },
+  beforeRouteEnter(to, from, next) {
+    db
+      .collection('pets')
+      .where('pet_id', '==', to.params.pet_id)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          next(vm => {
+            vm.pet_id = doc.data().pet_id;
+            vm.pet_name = doc.data().pet_name;
+            vm.dept = doc.data().dept;
+            vm.position = doc.data().position;
+          });
+        });
+      });
+  },
+  watch: {
+    $route: 'fetchData'
+  },
+  methods: {
+    fetchData() {
+      db
+        .collection('pets')
+        .where('pet_id', '==', this.$route.params.pet_id)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            this.pet_id = doc.data().pet_id;
+            this.pet_name = doc.data().pet_name;
+            this.dept = doc.data().dept;
+            this.position = doc.data().position;
+          });
+        });
+    },
+    deletePet() {
+      if (confirm('Are you sure?')) {
+        db
+          .collection('pets')
+          .where('pet_id', '==', this.$route.params.pet_id)
+          .get()
+          .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+              doc.ref.delete();
+              this.$router.push('/');
+            });
+          });
+      }
+    }
+  }
+};
+</script>
