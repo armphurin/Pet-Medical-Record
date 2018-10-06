9958af856a0a1c511fc6319326ee6f4d3fac3370
Sattaya Singkul <59070173@it.kmitl.ac.th>
2018-10-06 14:56:09 +0700

    firebase process sample

---
+++ vue_firestore_employeemanager-auth/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDQLpRy-xhEf65Ww2VSA3oRgxwVS13YG7E",
+  authDomain: "pet-medical-record-22d9a.firebaseapp.com",
+  databaseURL: "https://pet-medical-record-22d9a.firebaseio.com",
+  projectId: "pet-medical-record-22d9a",
+  storageBucket: "pet-medical-record-22d9a.appspot.com",
+  messagingSenderId: "89596850596"
+};
