934d56e8076041b3a2681942b868f6b82ad4fcfa
Thanadul <59070075@it.kmitl.ac.th>
2018-10-09 21:38:18 +0700

    Edit fire store config

--- Spike-Joe/Main/vue_pet/src/components/firebaseConfig.js
+++ Spike-Joe/Main/vue_pet/src/components/firebaseConfig.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: "AIzaSyAxdXzPMX7HkNcqTUTomIjVr3BbcwSvhc0",
-  authDomain: "pet-medical-record-5e936.firebaseapp.com",
-  databaseURL: "https://pet-medical-record-5e936.firebaseio.com",
-  projectId: "pet-medical-record-5e936",
-  storageBucket: "",
-  messagingSenderId: "808239707676"
+  apiKey: "AIzaSyDQLpRy-xhEf65Ww2VSA3oRgxwVS13YG7E",
+  authDomain: "pet-medical-record-22d9a.firebaseapp.com",
+  databaseURL: "https://pet-medical-record-22d9a.firebaseio.com",
+  projectId: "pet-medical-record-22d9a",
+  storageBucket: "pet-medical-record-22d9a.appspot.com",
+  messagingSenderId: "89596850596"
 };
