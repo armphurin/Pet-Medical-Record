c507806b7f4bb01e47e02dc41341a7d5ff897458
Phurin Pliensunthorn <59070137@it.kmitl.ac.th>
2018-10-10 22:08:32 +0700

    Edit register page

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -16,6 +16,36 @@
               <input type="password" id="password" v-model="password">
               <label for="password">Password</label>
             </div>
+            <div class="input-field">
+              <i class="material-icons prefix">person</i>
+              <input type="text" id="fullname" v-model="fullname">
+              <label for="text">Fullname</label>
+            </div>
+            <div class="input-field">
+              <i class="material-icons prefix">face</i>
+              <input type="text" id="age" v-model="age">
+              <label for="text">Age</label>
+            </div>
+            <div class="input-field">
+              <i class="material-icons prefix">wc</i>
+              <input type="text" id="gender" v-model="gender">
+              <label for="text">Gender</label>
+            </div>
+            <div class="input-field">
+              <i class="material-icons prefix">phone</i>
+              <input type="text" id="phone" v-model="phone">
+              <label for="text">Telephone</label>
+            </div>
+            <div class="input-field">
+              <i class="material-icons prefix">chat</i>
+              <input type="text" id="line" v-model="line">
+              <label for="text">Line ID</label>
+            </div>
+            <div class="input-field">
+              <i class="material-icons prefix">address</i>
+              <input type="text" id="address" v-model="address">
+              <label for="text">Address</label>
+            </div>
             <button v-on:click="register" class="btn btn-large btn-extended grey lighten-4 black-text">Register</button>
           </form>
         </div>
