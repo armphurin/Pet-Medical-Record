ec85915bcc9d30c541180e599d856bcebf45af0a
Gavin "James" M <59070010@it.kmitl.ac.th>
2018-11-09 13:13:29 +0700

    Front End Edit - "Spike" Responsive Homepage
    Spiking responsive homepage.

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -208,7 +208,7 @@ h6 {
 }
 
 /*Samsung Galaxy S5 Portrait*/
-@media only screen and (max-width: 360px) and (max-height: 640px) and (orientation: portrait) {
+@media only screen and (max-width: 360px) and (max-height: 700px) and (orientation: portrait) {
   .body-homepage {
     margin-top: 5em;
   }
@@ -223,8 +223,11 @@ h6 {
     height: 15em;
   }
   .svg-container-bottom {
-    bottom: -103em;
+    margin-bottom: -3em;
+    margin-left: -14px;
+    margin-right: -14px;
     height: 5em;
+    position: unset;
   }
 }
 
